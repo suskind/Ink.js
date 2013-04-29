db9a431afccbb21563ce2b6bc3594b4558cdf793
Ricardo Machado <ricardomachado@inovalabs.pt>
2013-04-29 21:27:37 +0100

    90% documented. To-Do :: Document Instances

--- Ink/UI/Aux/1/lib.js
+++ Ink/UI/Aux/1/lib.js
@@ -1,10 +1,6 @@
 /**
+ * @module Ink.UI.Aux_1
  * @author inkdev AT sapo.pt
- */
-
-
-/**
- * Auxiliar Utility functions to use with several components
  */
 Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.Selector_1','Ink.Util.Url_1'], function(Ajax,Css,Selector,Url) {
 
@@ -12,10 +8,24 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
 
     var instances = {};
     var lastIdNum = 0;
+
+    /**
+     * The Aux class provides auxiliar methods to ease some of the most common/repetitive UI tasks.
+     *
+     * @class Ink.UI.Aux
+     * @uses Ink.Net.Ajax_1
+     * @uses Ink.Dom.Css_1
+     * @uses Ink.Dom.Selector_1
+     * @uses Ink.Util.Url_1
+     */
     var Aux = {
 
         /**
-         * @property ? supported ink layouts
+         * Supported Ink Layouts
+         *
+         * @property Layouts
+         * @type Object
+         * @readOnly
          */
         Layouts: {
             SMALL:  's',
@@ -24,25 +34,55 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function {Boolean} ? returns true iif o is a DOM Element
-         * @param {any} o
+         * Method to check if an item is a valid DOM Element.
+         *
+         * @method isDOMElement
+         * @static
+         * @param {Mixed} o     The object to be checked.
+         * @return {Boolean}    True if it's a valid DOM Element.
+         * @example
+         *     var el = Ink.s('#element');
+         *     if( Ink.UI.Aux.isDOMElement( el ) === true ){
+         *         // It is a DOM Element.
+         *     } else {
+         *         // It is NOT a DOM Element.
+         *     }
          */
         isDOMElement: function(o) {
             return (typeof o === 'object' && 'nodeType' in o && o.nodeType === 1);
         },
 
         /**
-         * @function {Boolean} ? returns true iif n is an integer number
-         * @param {any} n
+         * Method to check if an item is a valid integer.
+         * 
+         * @method isInteger
+         * @static
+         * @param {Mixed} n     The value to be checked.
+         * @return {Boolean}    True if 'n' is a valid integer.
+         * @example
+         *     var value = 1;
+         *     if( Ink.UI.Aux.isInteger( value ) === true ){
+         *         // It is an integer.
+         *     } else {
+         *         // It is NOT an integer.
+         *     }
          */
         isInteger: function(n) {
             return (typeof n === 'number' && n % 1 === 0);
         },
 
         /**
-         * @function {DOMElement} ? applies the Sizzle selector/document.querySelectorAll if string, just returns element otherwise
-         * @param  {DOMElement|String} elOrSelector accepts a DOMElement or a CSS selector string
-         * @param  {String}            fieldName    the name the field occupies on the
+         * Method to get a DOM Element. The first parameter should be either a DOM Element or a valid CSS Selector.
+         * If not, then it will throw an exception. Otherwise, it returns a DOM Element.
+         * 
+         * @method elOrSelector
+         * @static
+         * @param  {DOMElement|String} elOrSelector Valid DOM Element or CSS Selector
+         * @param  {String}            fieldName    This field is used in the thrown Exception to identify the parameter.
+         * @return {DOMElement} Returns the DOMElement passed or the first result of the CSS Selector. Otherwise it throws an exception.
+         * @example
+         *     // In case there are several .myInput, it will retrieve the first found
+         *     var el = Ink.UI.Aux.elOrSelector('.myInput','My Input');
          */
         elOrSelector: function(elOrSelector, fieldName) {
             if (!this.isDOMElement(elOrSelector)) {
@@ -53,9 +93,22 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
             return elOrSelector;
         },
 
+
         /**
-         * @function {Object} ? returns a deep copy of an object. it can't have loops
-         * @param  {Object} o
+         * Method to make a deep copy (clone) of an object.
+         * Note: The object cannot have loops.
+         * 
+         * @method clone
+         * @static
+         * @param  {Object} o The object to be cloned/copied.
+         * @return {Object} Returns the result of the clone/copy.
+         * @example
+         *     var originalObj = {
+         *         key1: 'value1',
+         *         key2: 'value2',
+         *         key3: 'value3'
+         *     };
+         *     var cloneObj = Ink.UI.Aux.clone( originalObj );
          */
         clone: function(o) {
             try {
@@ -66,25 +119,53 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
             }
         },
 
+
         /**
-         * @function {Number} ? returns the nth position the given element occupies relatively to its parent
-         * @param  {DOMElement} childEl
+         * Method to return the 'nth' position that an element occupies relatively to its parent.
+         * 
+         * @method childIndex
+         * @static
+         * @param  {DOMElement} childEl Valid DOM Element.
+         * @return {Number} Numerical position of an element relatively to its parent.
+         * @example
+         *     <!-- Imagine the following HTML: -->
+         *     <ul>
+         *       <li>One</li>
+         *       <li>Two</li>
+         *       <li id="test">Three</li>
+         *       <li>Four</li>
+         *     </ul>
+         *
+         *     <script>
+         *         var testLi = Ink.s('#test');
+         *         Ink.UI.Aux.childIndex( testLi ); // Returned value: 3
+         *     </script>
          */
         childIndex: function(childEl) {
-            var els = Selector.select('> *', childEl.parentNode);
-            for (var i = 0, f = els.length; i < f; ++i) {
-                if (els[i] === childEl) {
-                    return i;
+            if( Aux.isDOMElement(childEl) ){
+                var els = Selector.select('> *', childEl.parentNode);
+                for (var i = 0, f = els.length; i < f; ++i) {
+                    if (els[i] === childEl) {
+                        return i;
+                    }
                 }
             }
             throw 'not found!';
         },
 
+
         /**
-         * @function ? for convenience, this methods does an AJAX request and offers a cb(err, jsonData) interface for easy async handling
-         * @param  {String}   endpoint the endpoint URI
-         * @param  {Object}   params   hash of param names -> values to pass
-         * @param  {Function} cb       function(err, data). null is passed on err if no err occurred.
+         * This method provides a more convenient way to do an async AJAX request and expect a JSON response.
+         * It offers a callback option, as third paramenter, for a better async handling.
+         * 
+         * @method ajaxJSON
+         * @static
+         * @async
+         * @param  {String} endpoint    Valid URL to be used as target by the request.
+         * @param  {Object} params      This field is used in the thrown Exception to identify the parameter.
+         * @example
+         *     // In case there are several .myInput, it will retrieve the first found
+         *     var el = Ink.UI.Aux.elOrSelector('.myInput','My Input');
          */
         ajaxJSON: function(endpoint, params, cb) {
             new Ajax(
@@ -113,8 +194,15 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
             );
         },
 
+
         /**
-         * @function {String} ? returns the current ink layout being applied
+         * Method to get the current Ink layout applied.
+         * 
+         * @method currentLayout
+         * @static
+         * @return {String}         Returns the value of one of the options of the property Layouts above defined.
+         * @example
+         *     var inkLayout = Ink.UI.Aux.currentLayout();
          */
         currentLayout: function() {
             var i, f, k, v, el, detectorEl = Selector.select('#ink-layout-detector')[0];
@@ -141,9 +229,20 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
             }
         },
 
+
         /**
-         * @function ? non-destructive set of hash parameters
-         * @param {Object} o object to index parameters and values to change on the hash. if other parameters were there, they are kept the same.
+         * Method to set the location's hash (window.location.hash).
+         * 
+         * @method hashSet
+         * @static
+         * @param  {Object} o   Object with the info to be placed in the location's hash.
+         * @example
+         *     // It will set the location's hash like: <url>#key1=value1&key2=value2&key3=value3
+         *     Ink.UI.Aux.hashSet({
+         *         key1: 'value1',
+         *         key2: 'value2',
+         *         key3: 'value3'
+         *     });
          */
         hashSet: function(o) {
             if (typeof o !== 'object') { throw new TypeError('o should be an object!'); }
@@ -152,12 +251,34 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
             window.location.hash = Url.genQueryString('', hashParams).substring(1);
         },
 
-
         /**
-         * @function ? IE8 doesn't like table.innerHTML = ''
-         * @param  {DOMElement} el
+         * Method to remove children nodes from a given object.
+         * This method was initially created to help solve a problem in Internet Explorer(s) that occurred when trying
+         * to set the innerHTML of some specific elements like 'table'.
+         * 
+         * @method cleanChildren
+         * @static
+         * @param  {DOMElement} parentEl Valid DOM Element
+         * @example
+         *     <!-- Imagine the following HTML: -->
+         *     <ul id="myUl">
+         *       <li>One</li>
+         *       <li>Two</li>
+         *       <li>Three</li>
+         *       <li>Four</li>
+         *     </ul>
+         *     
+         *     <script>
+         *     Ink.UI.Aux.cleanChildren( Ink.s( '#myUl' ) );
+         *     </script>
+         *     
+         *     <!-- After running it, the HTML changes to: -->
+         *     <ul id="myUl"></ul>
          */
         cleanChildren: function(parentEl) {
+            if( !Aux.isDOMElement(parentEl) ){
+                throw 'Please provide a valid DOMElement';
+            }
             var prevEl, el = parentEl.lastChild;
             while (el) {
                 prevEl = el.previousSibling;
@@ -167,11 +288,30 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function ? saves id and classes on fromEl as private attributes of inObj if present.
-         * @param  {DOMElement} fromEl
-         * @param  {Object}     inObj
+         * This method stores the id and/or the classes of a given element in a given object.
+         * 
+         * @method storeIdAndClasses
+         * @static
+         * @param  {DOMElement} fromEl    Valid DOM Element to get the id and classes from.
+         * @param  {Object}     inObj     Object where the id and classes will be saved.
+         * @example
+         *     <div id="myDiv" class="aClass"></div>
+         *     
+         *     <script>
+         *         var storageObj = {};
+         *         Ink.UI.Aux.storeIdAndClasses( Ink.s('#myDiv'), storageObj );
+         *         // storageObj changes to:
+         *         {
+         *           _id: 'myDiv',
+         *           _classes: 'aClass'
+         *         }
+         *     </script>
          */
         storeIdAndClasses: function(fromEl, inObj) {
+            if( !Aux.isDOMElement(fromEl) ){
+                throw 'Please provide a valid DOMElement as first parameter';
+            }
+
             var id = fromEl.id;
             if (id) {
                 inObj._id = id;
@@ -184,11 +324,34 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function ? recovers id and classes to toEl from private attributes of inObj if present.
-         * @param  {DOMElement} toEl
-         * @param  {Object}     inObj
+         * This method sets the id and className properties of a given DOM Element based on a given similar object
+         * resultant of the previous function 'storeIdAndClasses'.
+         * 
+         * @method restoreIdAndClasses
+         * @static
+         * @param  {DOMElement} toEl    Valid DOM Element to set the id and classes on.
+         * @param  {Object}     inObj   Object where the id and classes to be set are.
+         * @example
+         *     <div></div>
+         *     
+         *     <script>
+         *         var storageObj = {
+         *           _id: 'myDiv',
+         *           _classes: 'aClass'
+         *         };
+         *         
+         *         Ink.UI.Aux.storeIdAndClasses( Ink.s('div'), storageObj );
+         *     </script>
+         *     
+         *     <!-- After the code runs the div element changes to: -->
+         *     <div id="myDiv" class="aClass"></div>
          */
         restoreIdAndClasses: function(toEl, inObj) {
+
+            if( !Aux.isDOMElement(toEl) ){
+                throw 'Please provide a valid DOMElement as first parameter';
+            }
+
             if (inObj._id && toEl.id !== inObj._id) {
                 toEl.id = inObj._id;
             }
@@ -204,10 +367,13 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function ? saves component instance reference for later retrieval
-         * @param  {Object}           inst            JS object instance
-         * @param  {DOMElement}       el              DOM Element to associate with the JS object
-         * @param  {optional String}  optionalPrefix  defaults to 'instance'
+         * This method saves a component's instance reference for later retrieval.
+         * 
+         * @method registerInstance
+         * @static
+         * @param  {Object}     inst                Object that holds the instance.
+         * @param  {DOMElement} el                  DOM Element to associate with the object.
+         * @param  {Object}     [optionalPrefix]    Defaults to 'instance'
          */
         registerInstance: function(inst, el, optionalPrefix) {
             if (inst._instanceId) { return; }
@@ -227,26 +393,23 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function ? unregisters a component - useful only for volatile stuff such as modals
-         * @param {String} id
+         * This method deletes/destroy an instance with a given id.
+         * 
+         * @method unregisterInstance
+         * @static
+         * @param  {String}     id       Id of the instance to be destroyed.
          */
         unregisterInstance: function(id) {
             delete instances[id];
         },
 
         /**
-         * @function ? auxiliary method - returns registered instance from a CSS selector rule
-         * @param {String} selector
-         */
-        getInstanceFromSelector: function(selector) {
-            var el = Selector.select(selector)[0];
-            if (!el) { throw new Error('Element not found!'); }
-            return this.getInstance(el);
-        },
-
-        /**
-         * @function {Object|Object[]} ? returns an instance or several instances (if more than 1 instance is associated with the element)
-         * @param  {String|DOMElement} instanceIdOrElement
+         * This method retrieves the registered instance(s) of a given element or instance id.
+         * 
+         * @method getInstance
+         * @static
+         * @param  {String|DOMElement}      instanceIdOrElement      Instance's id or DOM Element from which we want the instances.
+         * @return  {Object|Object[]}       Returns an instance or a collection of instances.
          */
         getInstance: function(instanceIdOrElement) {
             var ids;
@@ -274,7 +437,25 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function {String[]} ? returns all registered instance ids
+         * This method retrieves the registered instance(s) of an element based on the given selector.
+         * 
+         * @method getInstanceFromSelector
+         * @static
+         * @param  {String}             selector    CSS selector to define the element from which it'll get the instance(s).
+         * @return  {Object|Object[]}               Returns an instance or a collection of instances.
+         */
+        getInstanceFromSelector: function(selector) {
+            var el = Selector.select(selector)[0];
+            if (!el) { throw new Error('Element not found!'); }
+            return this.getInstance(el);
+        },
+
+        /**
+         * This method retrieves the registered instances' ids of all instances.
+         * 
+         * @method getInstanceIds
+         * @static
+         * @return  {String[]}     Id or collection of ids of all existing instances.
          */
         getInstanceIds: function() {
             var res = [];
@@ -287,7 +468,11 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function {Object[]} ? returns all regitered instances
+         * This method retrieves all existing instances.
+         * 
+         * @method getInstances
+         * @static
+         * @return  {Object[]}     Collection of existing instances.
          */
         getInstances: function() {
             var res = [];
@@ -300,7 +485,11 @@ Ink.createModule('Ink.UI.Aux', '1', ['Ink.Net.Ajax_1','Ink.Dom.Css_1','Ink.Dom.S
         },
 
         /**
-         * @function ? not to be invoked from Aux. Components should copy this method as its destroy method.
+         * This method is not to supposed to be invoked by the Aux component.
+         * Components should copy this method as its destroy method.
+         * 
+         * @method destroyComponent
+         * @static
          */
         destroyComponent: function() {
             Ink.Util.Aux.unregisterInstance(this._instanceId);
