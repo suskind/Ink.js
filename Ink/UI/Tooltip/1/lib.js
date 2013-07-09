93ca8955b77b1139e0ba04cb00536e59ce139383
Fábio Santos <fabio-s-santos@telecom.pt>
2013-07-09 14:54:51 +0100

    boilerplate

---
+++ Ink/UI/Tooltip/1/lib.js
@@ -0,0 +1,11 @@
+Ink.createModule('Ink.UI.Tooltip', [], function () {
+    function Tooltip (element, options) {
+        this._init(element, options || {});
+    }
+
+    Tooltip.prototype = {
+        _init: function () {}
+    };
+
+    return Tooltip;
+});
