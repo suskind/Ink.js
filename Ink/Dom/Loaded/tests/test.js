3278e8b6cc23b89457f217bfe0c8c4aa4b66c8a6
Fábio Santos <fabio-s-santos@telecom.pt>
2013-07-19 18:01:39 +0100

    improved tests

--- Ink/Dom/Loaded/tests/test.js
+++ Ink/Dom/Loaded/tests/test.js
@@ -1,66 +1,64 @@
 /*globals equal,test,start,stop,expect*/
 Ink.requireModules(['Ink.Dom.Loaded_1'], function (Loaded) {
-    test('several Loaded callbacks called in order', function () {
-        stop();
+   test('several Loaded callbacks called in order', function () {
         expect(3);  // 3 assertions
+        stop(3);  // 3 start()s
         var i = 0;
         Loaded.run(function () {
-            equal(++i, 1);
+            equal(++i, 1, 'called first');
+            start();
         });
         Loaded.run(function () {
-            equal(++i, 2);
+            equal(++i, 2, 'called second');
+            start();
         });
-        setTimeout(function () {
-            Loaded.run(function () {
-                equal(++i, 3);
-                start();
-            });
-        }, 50);
+        Loaded.run(function () {
+            equal(++i, 3, 'called third');
+            start();
+        });
     });
     test('Several contexts', function () {
-        stop();
         expect(2);
+        stop(2);
         var iframe = document.createElement('iframe');
         iframe.src = 'iframe.html';
         document.getElementsByTagName('body')[0].appendChild(iframe);
         var iframeWindow = iframe.contentWindow;
+        console.log(iframeWindow)
         Loaded.run(iframeWindow, function () {
-            var that = this;
-            setTimeout(function () {
-                equal(that, iframeWindow); 
-            }, 50);
+            deepEqual(this, iframeWindow, 'being called with the iframe window');
+            start();
         });
         Loaded.run(window, function () {
-            var that = this;
-            setTimeout(function () {
-                equal(that, window);
-            }, 0);
+            deepEqual(this, window, 'being called with this window');
+            start();
         });
-        setTimeout(start, 100);
     });
     test('Nested calls', function () {
-        stop();
         expect(3);
+        stop(3); // start() gets called thrice
         var i = 0;
         Loaded.run(function () {
-            equal(++i, 1);
+            equal(++i, 1, 'called first');
+            start();
             Loaded.run(function () {
-                equal(++i, 2);
+                equal(++i, 2, 'called second');
+                start();
                 Loaded.run(function () {
-                    equal(++i, 3);
+                    equal(++i, 3, 'called third');
                     start();
                 });
             });
         });
     });
     test('When document is loaded, still wait for next tick', function () {
-        stop();
         expect(2);
+        stop();
         var i = 0;
         Loaded.run(function () {
-            equal(++i, 2);
+            equal(++i, 2, 'called second, after the function returns');
+            start();
         });
-        equal(++i, 1);
-        setTimeout(start, 100);
+        equal(++i, 1, 'called first');
     });
 });
